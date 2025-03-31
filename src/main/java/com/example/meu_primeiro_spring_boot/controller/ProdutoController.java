eaa0f1d81ebf055aacda459745b91ac50dd530b5
Bruno <rotcodmwol@gmail.com>
2025-03-31 20:34:35 -0300

    Novo sistema de exception generic :)

--- src/main/java/com/example/meu_primeiro_spring_boot/controller/ProdutoController.java
+++ src/main/java/com/example/meu_primeiro_spring_boot/controller/ProdutoController.java
@@ -38,13 +38,8 @@ public class ProdutoController {
 
     @GetMapping("/{id}")
     public ResponseEntity<?> buscarProduto(@PathVariable Long id) {
-        try {
-            Produto produto = produtoServices.buscarPorId(id);
-            return ResponseEntity.ok(produto);
-        }
-        catch (RecursoNaoEncontradoException e){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        }
+        Produto produto = produtoServices.buscarPorId(id);
+        return ResponseEntity.ok(produto);
     }
 
     @PostMapping()
